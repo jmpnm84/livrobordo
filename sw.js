483e9dc2b3de0b40cd850c259356279de6835dad
jmpnm84 <jmpnm84@gmail.com>
2026-07-15 09:43:58 +0100

    Update service worker to use new cache strategy

--- sw.js
+++ sw.js
@@ -1,6 +1,14 @@
+const CACHE_NAME = 'manutencao-v1';
+const ASSETS = ['./', './index.html'];
+
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open('pwa-store').then((cache) => cache.addAll(['index.html'])));
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+});
+
 self.addEventListener('fetch', (e) => {
     e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
